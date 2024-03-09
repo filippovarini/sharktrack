74d50e4c19ca668fed10be5814039523670291e4
Filippo Varini <filippo.varini20@imperial.ac.uk>
2024-03-09 19:51:41 +0000

    compute maxN from viame output

--- scripts/clean_annotations.py
+++ scripts/clean_annotations.py
@@ -1,5 +1,5 @@
-#%%
 import pandas as pd
+from argparse import ArgumentParser
 import os
 
 def clean_annotations(output_path, viame_output_path):
@@ -17,6 +17,29 @@ def clean_annotations(output_path, viame_output_path):
 
     return sharktrack_df
 
-# if __name__ == "__main__":
-    # df = clean_annotations("output", "output/test123.csv")
-# %%
+
+def compute_species_max_n(cleaned_annotations):
+    frame_box_cnt = cleaned_annotations.groupby(["video", "chapter", "time", "class"], as_index=False)["track_id"].count()
+    frame_box_cnt.rename({"track_id": "n", "time": "time_sample"}, axis=1, inplace=True)
+
+    # for each video, species, get the max n and return video, species, max_n, chapter, time when that happens
+    max_n = frame_box_cnt.sort_values("n", ascending=False).groupby(["video", "class"], as_index=False).head(1)
+    max_n = max_n.sort_values(["video", "n"], ascending=[True, False])
+    max_n = max_n.reset_index(drop=True)
+
+    return max_n
+
+def main(output_path, viame_output_path):
+    cleaned_annotations = clean_annotations(output_path, viame_output_path)
+    max_n = compute_species_max_n(cleaned_annotations)
+
+    max_n_path = os.path.join(output_path, "max_n.csv")
+    max_n.to_csv(max_n_path, index=False)
+
+
+if __name__ == "__main__":
+    parser = ArgumentParser()
+    parser.add_argument("-o", type=str, required=True, help="Path to the output folder of sharktrack")
+    parser.add_argument("-v", type=str, required=True, help="Path to the output folder of viame")
+    args = parser.parse_args()
+    main(args.o, args.v)
