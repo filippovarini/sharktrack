a80c05859cbabf09e488e10647e002250e4c99f9
Filippo Varini <filippo.varini20@imperial.ac.uk>
2024-04-04 20:41:59 +0100

    live stream

--- app.py
+++ app.py
@@ -31,14 +31,15 @@ class Model():
     analyst_model = "models/analyst.pt"
     if mobile:
       print("Using mobile model...")
-      self.model_path = mobile_model
+      self.model = YOLO(mobile_model)
       self.tracker_path = "trackers/tracker_1fps.yaml"
       self.run_tracker = self.track_frames
       self.fps = 1
     else:
       print("Using analyst model...")
-      self.model_path = analyst_model
+      self.model = YOLO(analyst_model)
       self.tracker_path = "trackers/tracker_5fps.yaml"
+      self.tracker_path = "botsort.yaml"
       self.run_tracker = self.track_video
       self.fps = 5
     
@@ -47,6 +48,16 @@ class Model():
     self.iou_association_threshold = 0.5
     self.imgsz = 640
     self.device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+
+    self.model_args = {
+          "conf": self.conf_threshold,
+          "iou": self.iou_association_threshold,
+          "imgsz": self.imgsz,
+          "tracker": self.tracker_path,
+          "verbose": False,
+          "device":self.device
+      }
+
 
     # config
     self.next_track_index = 0
@@ -68,7 +79,6 @@ class Model():
     Tracks keyframes using PyAv to overcome the GoPro audio format issue.
     """
     print(f"Processing video: {chapter_path}... on device {self.device}")
-    model = YOLO(self.model_path)
     results = []
 
     content = av.datasets.curated(chapter_path)
@@ -78,17 +88,13 @@ class Model():
       video_stream.codec_context.skip_frame = 'NONKEY'  # and only keyframes (1fps)
 
       for frame in container.decode(video_stream):
-        frame_results = model.track(
+        frame_results = self.model.track(
           source=frame.to_image(),
-          conf=self.conf_threshold,
-          iou=self.iou_association_threshold,
-          imgsz=self.imgsz,
-          tracker=self.tracker_path,
-          verbose=False,
+          **self.model_args,
           persist=True,
         )
         results.append(frame_results[0])
-
+        
     return results
 
   def track_video(self, chapter_path):
@@ -97,21 +103,49 @@ class Model():
     This is faster but it fails with GoPro Audio format, requiring reformatting.
     """
     print(f"Processing video: {chapter_path}... on device {self.device}")
-    model = YOLO(self.model_path)
 
-    results = model.track(
+    results = self.model.track(
       chapter_path,
-      conf=self.conf_threshold,
-      iou=self.iou_association_threshold,
-      imgsz=self.imgsz,
-      tracker=self.tracker_path,
+      **self.model_args,
       vid_stride=self._get_frame_skip(chapter_path),
-      verbose=False,
       stream=True,
-      device=self.device
     )
 
     return results
+
+  def live_track(self, chapter_path, output_folder='./output/'):
+    """
+    Plot the live tracking video for debugging purposes
+    """
+    print("Live tracking video...")
+    assert valid_video(chapter_path), f"⚠️ Live Tracking is heavy so it can be done on only one video at a time. Please provide a valid video path."
+    cap = cv2.VideoCapture(chapter_path)
+
+    filename = chapter_path.split('/')[-1]
+    FILE_OUTPUT = f"{output_folder}{filename.split('.')[0]}_tracked.avi"
+    frame_width = int(cap.get(3))
+    frame_height = int(cap.get(4))
+
+    out = cv2.VideoWriter(FILE_OUTPUT, cv2.VideoWriter_fourcc('M','J','P','G'),
+                            30, (frame_width, frame_height))
+
+    while cap.isOpened():
+      success, frame = cap.read()
+
+      if success:
+        results = self.model.track(frame, **self.model_args, persist=True)
+        annotated_frame = results[0].plot()
+
+        out.write(annotated_frame)
+
+        if cv2.waitKey(1) & 0xFF == ord("q"):
+            break
+      else:
+        break
+
+    cap.release()
+    cv2.destroyAllWindows()
+
 
   def run(self):
     # remove previous predictions first
@@ -123,17 +157,23 @@ class Model():
     self.videos_folder = self.videos_folder
     processed_videos = []
 
-    for (root, _, files) in os.walk(self.videos_folder):
-      for file in files:
-        if len(processed_videos) == self.max_video_cnt:
-          break
-        stereo_filter = self.stereo_prefix is None or file.startswith(self.stereo_prefix)
-        if valid_video(file) and stereo_filter:
-          chapter_path = os.path.join(root, file)
-          chapter_id = chapter_path.replace(f"{self.videos_folder}/", "")
-          chapter_results = self.run_tracker(chapter_path)
-          self.save_chapter_results(chapter_id, chapter_results)
-          processed_videos.append(chapter_path)
+    if valid_video(self.videos_folder):
+      print("processing only one video...")
+      chapter_results = self.run_tracker(self.videos_folder)
+      self.save_chapter_results(self.videos_folder, chapter_results)
+      processed_videos.append(self.videos_folder)
+    else:
+      for (root, _, files) in os.walk(self.videos_folder):
+        for file in files:
+          if len(processed_videos) == self.max_video_cnt:
+            break
+          stereo_filter = self.stereo_prefix is None or file.startswith(self.stereo_prefix)
+          if valid_video(file) and stereo_filter:
+            chapter_path = os.path.join(root, file)
+            chapter_id = chapter_path.replace(f"{self.videos_folder}/", "")
+            chapter_results = self.run_tracker(chapter_path)
+            self.save_chapter_results(chapter_id, chapter_results)
+            processed_videos.append(chapter_path)
 
     if len(processed_videos) == 0:
       print("No chapters found in the given folder")
@@ -149,7 +189,7 @@ class Model():
 
     return processed_videos
 
-def main(video_path, max_video_cnt, stereo_prefix, output_path='./output', mobile=False):
+def main(video_path, max_video_cnt, stereo_prefix, output_path='./output', mobile=False, live=False):
   model = Model(
     video_path,
     max_video_cnt,
@@ -157,7 +197,10 @@ def main(video_path, max_video_cnt, stereo_prefix, output_path='./output', mobil
     output_path,
     mobile
   )
-  model.run()
+  if live:
+    model.live_track(video_path)
+  else:
+    model.run()
   
 
 if __name__ == "__main__":
@@ -167,5 +210,6 @@ if __name__ == "__main__":
   parser.add_argument("--max_videos", type=int, default=1000, help="Maximum videos to process")
   parser.add_argument("--output", type=str, default="./output", help="Output directory for the results")
   parser.add_argument("--mobile", action="store_true", help="Use mobile model: 50% faster, slightly less accurate than humans")
+  parser.add_argument("--live", action="store_true", help="Show live tracking video for debugging purposes")
   args = parser.parse_args()
-  main(args.input, args.max_videos, args.stereo_prefix, args.output, args.mobile)
+  main(args.input, args.max_videos, args.stereo_prefix, args.output, args.mobile, args.live)
