e51aca04dc4dd57f9fd613bb7a1042dba0d70b56
Filippo Varini <filippo@alpha10x.com>
2024-04-08 19:23:34 -0700

    refactor save maxn

--- compute_output/image_processor.py
+++ compute_output/image_processor.py
@@ -2,25 +2,14 @@ import cv2
 import numpy as np
 
 
-def draw_bboxes(image, bboxes, max_conf_bbox):
+def draw_bbox(image, bbox, color=(0, 255, 0)):
     """
-    Draw bounding boxes on an image
-    :param image: image to draw the bounding boxes on
-    :param bboxes: list of bounding boxes
-    :param color: color of the bounding boxes
-    :param max_conf_bbox: maximum confidence bounding box which we want to highlight
-    :return: image with bounding boxes drawn
     """
     img = image.copy()
-    for bbox in bboxes:
-        color= (255, 255, 255)
-        thickness = 1
-        if (bbox == max_conf_bbox).all():
-            color = (0, 0, 255)
-            thickness = 2
-        bbox = np.array(bbox).astype(int)
-        pt1, pt2 = (bbox[0], bbox[1]), (bbox[2], bbox[3])
-        img = cv2.rectangle(img, pt1, pt2, color, thickness)
+    thickness = 2
+    bbox = np.array(bbox).astype(int)
+    pt1, pt2 = (bbox[0], bbox[1]), (bbox[2], bbox[3])
+    img = cv2.rectangle(img, pt1, pt2, color, thickness)
     return img
 
 def annotate_image(img, chapter_path, time, conf):
