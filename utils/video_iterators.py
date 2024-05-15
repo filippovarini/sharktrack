2d05d8621065decf538db026f7df6b7758229a1a
Filippo Varini <filippo@alpha10x.com>
2024-05-15 14:11:50 -0700

    reflect changes into pyav iterator

--- utils/video_iterators.py
+++ utils/video_iterators.py
@@ -1,6 +1,10 @@
 import cv2
+import av
 
 def stride_iterator(video_path, vid_stride):
+    """
+    Iterates with cv2 each vid_stride frame
+    """
     vidcap = cv2.VideoCapture(video_path)
     ret, frame = vidcap.read()
     n = 0
@@ -16,4 +20,16 @@ def stride_iterator(video_path, vid_stride):
                 break
             
 
-        
+def keyframe_iterator(video_path):
+    """
+    Iterates quickier with pyav each keyframe
+    """
+    content = av.datasets.curated(video_path)
+    with av.open(content) as container:
+      video_stream = container.streams.video[0]         # take only video stream
+      video_stream.codec_context.skip_frame = 'NONKEY'  # and only keyframes (1fps)
+      frame_idx = 0
+      for frame in container.decode(video_stream):
+        time_ms = round((frame.pts * video_stream.time_base) * 1000)
+        yield frame.to_image(), time_ms, frame_idx
+        frame_idx += 1
