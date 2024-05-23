569213ba398fbc169bb2a0b3e952bd8fba3a183d
Filippo Varini <filippo@alpha10x.com>
2024-05-23 10:04:10 -0700

    vid iterator with queue

--- utils/video_iterators.py
+++ utils/video_iterators.py
@@ -3,54 +3,46 @@ import os
 import subprocess
 import tempfile
 import time
+import av
 
-def _extract_frames_ffmpeg(video_path, output_video_path, vid_stride):
-    """
-    Uses FFmpeg to extract every vid_stride frame and save to output_video_path.
-    Captures and displays FFmpeg output if show_log is True.
-    """
-    ffmpeg_cmd = [
-        'ffmpeg', '-i', video_path,
-        '-an',
-        '-vf', f'select=not(mod(n\,{vid_stride}))',
-        '-fps_mode', 'vfr',
-        '-c:v', 'libx264',
-        '-preset', 'ultrafast',
-        output_video_path
-    ]
-    ffmpeg_printed_cmd = ' \\\n    '.join(ffmpeg_cmd)
-    print(f'Running ffmpeg command:\n$ {ffmpeg_printed_cmd}')
-    subprocess.run(ffmpeg_cmd, stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
+import cv2
+import time
+import threading
+from queue import Queue
 
-def _stride_iterator(video_path, video_stride):
-    """
-    Iterates with cv2 each vid_stride frame from a .mkv file.
-    """
-    print(f'Started iterating over {video_path} with stride {video_stride}')
-    with tempfile.TemporaryDirectory() as tmpdirname:
-        output_video_path = os.path.join(tmpdirname, 'output.mkv')
-
-        start_time = time.time()
-        _extract_frames_ffmpeg(video_path, output_video_path, video_stride)
-        elapsed_time = time.time() - start_time
-        print(f"FFmpeg processing time: {elapsed_time:.2f} seconds")
-
-        vidcap = cv2.VideoCapture(output_video_path)
-        original_frame_index = 0
-        while vidcap.isOpened():
-            ret, frame = vidcap.read()
-            if not ret:
+def frame_producer(video_path, vid_stride, frame_queue):
+    vidcap = cv2.VideoCapture(video_path, cv2.CAP_FFMPEG)
+    n = 0
+    while vidcap.isOpened():
+        n += 1
+        vidcap.grab()
+        if n % vid_stride == 0:
+            ret, frame = vidcap.retrieve()
+            if ret:
+                a = vidcap.get(cv2.CAP_PROP_POS_MSEC)
+                b = vidcap.get(cv2.CAP_PROP_POS_FRAMES)
+                frame_queue.put((frame, a, b))
+            else:
                 break
+    vidcap.release()
+    frame_queue.put(None)
 
-            # TODO: this might be off by a few milliseconds. Need to improve it.
-            original_frame_time = vidcap.get(cv2.CAP_PROP_POS_MSEC) * video_stride
-            yield frame, original_frame_time, original_frame_index
-            original_frame_index += video_stride
-        vidcap.release()
-
-
-# TODO: Remove this function once the new one is tested
 def stride_iterator(video_path, vid_stride):
+    print("Using stride iterator with queue")
+    frame_queue = Queue(maxsize=100)
+    producer_thread = threading.Thread(target=frame_producer, args=(video_path, vid_stride, frame_queue))
+    producer_thread.start()
+
+    while True:
+        item = frame_queue.get()
+        if item is None:
+            break
+        frame, a, b = item
+        yield frame, a, b
+    
+    producer_thread.join()
+
+def depricated_stride_iterator(video_path, vid_stride):
     """
     Iterates with cv2 each vid_stride frame
     """
