2b1c4bc05961da22c1a6caccd74238cc6a7f669d
Filippo Varini <filippo@alpha10x.com>
2024-09-23 15:51:01 +0200

    test image processor

--- tests/test_time_processor.py
+++ tests/test_time_processor.py
@@ -2,23 +2,32 @@ import unittest
 from time_processor import ms_to_string, string_to_ms
 
 class TestGenerateCorrectOutput(unittest.TestCase):
-    def test__convert_time_correctly(self):
-        matches = [
-            (1000, "00h:00m:01s:000ms"),
-            (1001, "00h:00m:01s:001ms"),
-            (3600000, "01h:00m:00s:000ms"),
-            (3600200, "01h:00m:00s:200ms"),
-            (2335, "00h:00m:02s:335ms")
-        ]
+    def test_ms_to_string(self):
+        # Basic tests
+        self.assertEqual(ms_to_string(0), "00h:00m:00s:000ms")
+        self.assertEqual(ms_to_string(1000), "00h:00m:01s:000ms")
+        self.assertEqual(ms_to_string(61000), "00h:01m:01s:000ms")
+        self.assertEqual(ms_to_string(3600000), "01h:00m:00s:000ms")
+        self.assertEqual(ms_to_string(3661000), "01h:01m:01s:000ms")
 
-        for ms, ms_string in matches:
-            self.assertEqual(ms_to_string(ms), ms_string)    
-            self.assertEqual(ms_to_string(ms), ms_string)    
-            print(ms)
+        # Edge cases
+        self.assertEqual(ms_to_string(1), "00h:00m:00s:001ms")
+        self.assertEqual(ms_to_string(999), "00h:00m:00s:999ms")
+        self.assertEqual(ms_to_string(86399999), "23h:59m:59s:999ms")  # Max time in a day
+
+    def test_string_to_ms(self):
+        # Basic tests
+        self.assertEqual(string_to_ms("00h:00m:00s:000ms"), 0)
+        self.assertEqual(string_to_ms("00h:00m:01s:000ms"), 1000)
+        self.assertEqual(string_to_ms("00h:01m:01s:000ms"), 61000)
+        self.assertEqual(string_to_ms("01h:00m:00s:000ms"), 3600000)
+        self.assertEqual(string_to_ms("01h:01m:01s:000ms"), 3661000)
+
+        # Edge cases
+        self.assertEqual(string_to_ms("00h:00m:00s:001ms"), 1)
+        self.assertEqual(string_to_ms("00h:00m:00s:999ms"), 999)
+        self.assertEqual(string_to_ms("23h:59m:59s:999ms"), 86399999)
 
     
-    
-
-
 if __name__ == '__main__':
     unittest.main()
